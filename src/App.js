c9773cd6266f56e88f804e347aee951f15dc7f5b
shivam maurya <shivam@innotech.in>
2022-04-25 19:24:32 +0530

    improve npm bootstrap and others

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [startBtn, setStartBtn] = useState(false);
   const [time, setTime] = useState(0);
 
   useEffect(() => {
     let interval = null;
 
-    if (isActive === false) {
+    if (startBtn) {
       interval = setInterval(() => {
-        setTime((time) => time + 10);
-      }, 10);
+        setTime((time) => time + 1);
+      }, 100);
     } else {
-      clearInterval(interval);
+      clearInterval(!interval);
     }
     return () => {
       clearInterval(interval);
     };
-  }, [isActive]);
+  }, [startBtn]);
 
   const handleStart = () => {
-    if(isActive===true){
-      setIsActive(false)
-    }else{
-      setIsActive(true)
+    if (startBtn) {
+      setStartBtn(false);
+    } else {
+      setStartBtn(true);
     }
   };
 
@@ -33,14 +33,14 @@ const App = () => {
       <div className="container d-flex justify-content-center align-items-center ">
         <button
           type="button"
-          class="btn btn-primary mainbtn"
+          className="btn btn-primary mainbtn"
           data-bs-toggle="modal"
           data-bs-target="#staticBackdrop"
         >
           Start
         </button>
         <div
-          class="modal fade"
+          className="modal fade"
           id="staticBackdrop"
           data-bs-backdrop="static"
           data-bs-keyboard="false"
@@ -48,39 +48,33 @@ const App = () => {
           aria-labelledby="staticBackdropLabel"
           aria-hidden="true"
         >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="staticBackdropLabel">
                   Counter App
                 </h5>
-                <button
-                  type="button"
-                  class="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
               </div>
               <div class="modal-body">
                 <div className="timer">
                   <span className="digits">
-                    {("00" + Math.floor((time / 60000) % 60)).slice(-2)}:
+                    {("0" + Math.floor((time / 6000) % 60)).slice(-2)}:
                   </span>
                   <span className="digits">
-                    {("00" + Math.floor((time / 1000) % 60)).slice(-2)}: 
+                    {("0" + Math.floor((time / 100) % 60)).slice(-2)}:
                   </span>
                   <span className="digits mili-sec">
-                    {("00" + ((time / 10) % 100)).slice(-2)}
+                    {("0" + (time % 100)).slice(-2)}
                   </span>
                 </div>
               </div>
-              <div class="modal-footer mx-auto">
+              <div className="modal-footer mx-auto">
                 <button
                   type="button"
-                  class="btn btn-dark mainbtn"
+                  className="btn btn-dark mainbtn"
                   onClick={handleStart}
                 >
-                 {isActive ? "start" : "stop"}  
+                  {!startBtn ? "start" : "stop"}
                 </button>
               </div>
             </div>
